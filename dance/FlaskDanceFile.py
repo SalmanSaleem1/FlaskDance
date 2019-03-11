32c1472d4e3c2a24d368c6fef8a53a41c12adce3
Salman Saleem <salman.saleem@southvillesol.com>
2019-03-11 17:46:14 +0000

    Init project

---
+++ dance/FlaskDanceFile.py
@@ -0,0 +1,45 @@
+from flask import Flask, redirect, url_for
+from flask_dance.contrib.twitter import make_twitter_blueprint, twitter
+from flask_dance.contrib.github import make_github_blueprint, github
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thisissupposedtobeasecret'
+
+twitter_blueprint = make_twitter_blueprint(api_key='API_KEY', api_secret='API_SECRET')
+
+github_blueprint = make_github_blueprint(client_id='f0323a1f42716fb0520f', client_secret='9c554ca9e75f6b42732773949945ca2222e7092f')
+
+app.register_blueprint(twitter_blueprint, url_prefix='/twitter_login')
+
+app.register_blueprint(github_blueprint, url_prefix='/github_login')
+
+@app.route('/twitter')
+def twitter_login():
+    if not twitter.authorized:
+        return redirect(url_for('twitter.login'))
+    account_info = twitter.get('account/settings.json')
+
+    if account_info.ok:
+        account_info_json = account_info.json()
+
+        return '<h1>Your Twitter name is @{}'.format(account_info_json['screen_name'])
+
+    return '<h1>Request failed!</h1>'
+
+@app.route('/github')
+def github_login():
+    if not github.authorized:
+        return redirect(url_for('github.login'))
+
+    account_info = github.get('/user')
+
+    if account_info.ok:
+        account_info_json = account_info.json()
+
+        return '<h1>Your Github name is {}'.format(account_info_json['login'])
+
+    return '<h1>Request failed!</h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
